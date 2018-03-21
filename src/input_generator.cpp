9c5077258d2736278103e5b4fbf620b7c83bbd80
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-03-21 20:23:43 +0100

    Optimize special case in input generator

--- src/input_generator.cpp
+++ src/input_generator.cpp
@@ -2,7 +2,8 @@
 
 #include <cassert>
 
-static std::string random_string(size_t n, const std::string& set) {
+static
+std::string random_string(size_t n, const std::string& set) {
     
     std::string result(n, ' ');
     for (size_t i=0; i < n; i++) {
@@ -12,10 +13,38 @@ static std::string random_string(size_t n, const std::string& set) {
     return result;
 }
 
+static
+std::string generate(size_t size, size_t longest_number) {
+
+    std::string result;
+
+    static const std::string numbers    = "0123456789";
+    static const std::string separators = ",; ";
+
+    while (true) {
+        const size_t n = std::max(size_t(1), rand() % longest_number);
+        const size_t k = 1;
+
+        const std::string number = random_string(n, numbers);
+
+        if (result.size() + n + k < size) {
+            result += number;
+            result += separators[0];
+        } else {
+            result += random_string(size - result.size(), separators);
+            return result;
+        }
+    }
+}
+
 std::string generate(size_t size, size_t longest_number, size_t longest_separator) {
 
     assert(longest_number > 0);
     assert(longest_separator > 0);
+
+    if (longest_separator == 1) {
+        return generate(size, longest_number);
+    }
 
     std::string result;
 
