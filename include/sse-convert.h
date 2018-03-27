e83b630fd7167909c6932613c9bbff1f9c3721a1
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-03-27 21:38:11 +0200

    Preliminary verification of signed parsing

--- include/sse-convert.h
+++ include/sse-convert.h
@@ -78,6 +78,74 @@ namespace sse {
             *output++ = tmp[i];
     }
 
+    template <typename INSERTER>
+    void convert_2digits_signed(const __m128i& input, const __m128i& negate_mask, int count, INSERTER output) {
+        const __m128i ascii0   = _mm_set1_epi8('0');
+        const __m128i mul_1_10 = _mm_setr_epi8(10, 1, 10, 1, 10, 1, 10, 1, 10, 1, 10, 1, 10, 1, 10, 1);
+
+        const __m128i t0 = _mm_subs_epu8(input, ascii0);
+        const __m128i t1 = _mm_maddubs_epi16(t0, mul_1_10);
+
+        const __m128i s0 = _mm_xor_si128(t1, negate_mask);
+        const __m128i s1 = _mm_sub_epi16(s0, negate_mask);
+
+        int16_t tmp[8] SSE_ALIGN;
+
+        _mm_store_si128((__m128i*)tmp, s1);
+        for (int i=0; i < count; i++)
+            *output++ = tmp[i];
+    }
+
+    template <typename INSERTER>
+    void convert_4digits_signed(const __m128i& input, const __m128i& negate_mask, int count, INSERTER output) {
+
+        const __m128i ascii0      = _mm_set1_epi8('0');
+        const __m128i mul_1_10    = _mm_setr_epi8(10, 1, 10, 1, 10, 1, 10, 1, 10, 1, 10, 1, 10, 1, 10, 1);
+        const __m128i mul_1_100   = _mm_setr_epi16(100, 1, 100, 1, 100, 1, 100, 1); 
+
+        // =--------------
+
+        const __m128i t0 = _mm_subs_epu8(input, ascii0);
+        const __m128i t1 = _mm_maddubs_epi16(t0, mul_1_10);
+        const __m128i t2 = _mm_madd_epi16(t1, mul_1_100);
+
+        const __m128i s0 = _mm_xor_si128(t2, negate_mask);
+        const __m128i s1 = _mm_sub_epi32(s0, negate_mask);
+
+        int32_t tmp[4] SSE_ALIGN;
+
+        _mm_store_si128((__m128i*)tmp, s1);
+        for (int i=0; i < count; i++)
+            *output++ = tmp[i];
+    }
+
+    template <typename INSERTER>
+    void convert_8digits_signed(const __m128i& input, const __m128i& negate_mask, int count, INSERTER output) {
+
+        const __m128i ascii0      = _mm_set1_epi8('0');
+        const __m128i mul_1_10    = _mm_setr_epi8(10, 1, 10, 1, 10, 1, 10, 1, 10, 1, 10, 1, 10, 1, 10, 1);
+        const __m128i mul_1_100   = _mm_setr_epi16(100, 1, 100, 1, 100, 1, 100, 1); 
+        const __m128i mul_1_10000 = _mm_setr_epi16(10000, 1, 10000, 1, 10000, 1, 10000, 1);
+
+        // =--------------
+
+        const __m128i t0 = _mm_subs_epu8(input, ascii0);
+        const __m128i t1 = _mm_maddubs_epi16(t0, mul_1_10);
+        const __m128i t2 = _mm_madd_epi16(t1, mul_1_100);
+
+        const __m128i s0 = _mm_xor_si128(t2, negate_mask);
+        const __m128i s1 = _mm_sub_epi32(s0, negate_mask);
+        
+        const __m128i t3 = _mm_packs_epi32(s1, s1);
+        const __m128i t4 = _mm_madd_epi16(t3, mul_1_10000);
+
+        int32_t tmp[4] SSE_ALIGN;
+
+        _mm_store_si128((__m128i*)tmp, t4);
+        for (int i=0; i < count; i++)
+            *output++ = tmp[i];
+    }
+
 #undef SSE_ALIGN
 
 }
