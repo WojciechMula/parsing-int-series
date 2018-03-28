52968f5d8e5ab8910ed6b95daf2825f971fbe85c
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-03-28 21:16:38 +0200

    Add a helper function

--- include/sse/sse-utils.h
+++ include/sse/sse-utils.h
@@ -12,4 +12,23 @@ namespace sse {
         return _mm_andnot_si128(t0, t1); // x <= '9' and x >= '0'
     }
 
+    __m128i from_epu16(const uint16_t x, uint8_t one = 0xff) {
+
+        uint8_t tmp[16];
+        memset(tmp, 0, sizeof(tmp));
+
+        int idx = 0;
+        uint16_t val = x;
+        while (val) {
+            if (val & 0x0001) {
+                tmp[idx] = one;
+            }
+
+            val >>= 1;
+            idx += 1;
+        }
+
+        return _mm_loadu_si128((const __m128i*)tmp);
+    }
+
 }
