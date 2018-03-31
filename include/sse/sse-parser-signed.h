91f44400513f7241eb71d9c80c92fca17bf5f4e6
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-03-31 16:53:42 +0200

    Change order of computations

--- include/sse/sse-parser-signed.h
+++ include/sse/sse-parser-signed.h
@@ -28,15 +28,15 @@ namespace sse {
             const __m128i bytemask_sign  = _mm_or_si128(bytemask_plus, bytemask_minus);
             const __m128i bytemask_span  = _mm_or_si128(bytemask_digit, bytemask_sign);
 
-            const uint16_t sign_mask     = _mm_movemask_epi8(bytemask_sign);
-            const uint16_t span_mask     = _mm_movemask_epi8(bytemask_span);
             const uint16_t valid_mask    = _mm_movemask_epi8(matcher.get_mask(input, bytemask_span));
 
             if (valid_mask != 0xffff) {
                 throw std::runtime_error("Wrong character");
             }
 
-            const BlockInfo& bi = blocks[span_mask];
+            const uint16_t sign_mask = _mm_movemask_epi8(bytemask_sign);
+            const uint16_t span_mask = _mm_movemask_epi8(bytemask_span);
+            const BlockInfo& bi      = blocks[span_mask];
             if (sign_mask & bi.invalid_sign_mask) {
                 throw std::runtime_error("'+' or '-' at invalid position");
             }
