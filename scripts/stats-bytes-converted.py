ef5e3449c2258872e5e9dd772bb1df8c43845383
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-04-05 23:18:55 +0200

    Statistics of total bytes processed

---
+++ scripts/stats-bytes-converted.py
@@ -0,0 +1,28 @@
+from generator import Generator
+from table import Table
+
+if __name__ == '__main__':
+    gen = Generator()
+
+    freq = {}
+    for bi in gen.run():
+        k = bi.total_skip
+
+        freq[k] = freq.get(k, 0) + 1
+
+    table = Table()
+    table.add_header(["bytes processed", "patterns", "%", "cumulative %"])
+
+    cumulative = 0
+    for total_skip in sorted(freq.keys()):
+        count = freq[total_skip]
+        cumulative += count
+
+        table.add_row([
+            '%d' % total_skip,
+            '%d' % count,
+            '%0.2f%%' % (100 * count/65536.0),
+            '%0.2f%%' % (100 * cumulative/65536.0),
+        ])
+
+    print table
